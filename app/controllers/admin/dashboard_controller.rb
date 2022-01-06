a48a32ba8445f565b6978ba84fbae835793fee4e
Kenneth Chen <kenchen0606@gmail.com>
2022-01-06 05:51:54 +0000

    add a dashboard page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,25 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @category = Category.all
+    @product = Product.all
+
+    @category_count = []
+
+    @category.each do |cat|
+      @product_by_category = Product.where(category_id: cat.id).count
+
+      @category_count.push({
+        name: cat.name,
+        count: @product_by_category
+      })
+    end
+
+    @category_count.push({
+      name: "Total",
+      count: Product.count
+    })
+
+    puts @category.inspect
+    puts @category_count
   end
 end
