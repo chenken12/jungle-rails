e93ce52307a9f1305b9b5c201d3ddb739d8e56a9
Kenneth Chen <kenchen0606@gmail.com>
2022-01-11 04:21:14 +0000

    check validates errors

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -9,10 +9,7 @@ RSpec.describe Product, type: :model do
     end
 
     it 'should save new product in the db' do
-      @category = Category.new({
-        name: "Spec"
-      })
-      @category.save!
+      @category = Category.create({name: "Spec"})
       
       @product = Product.new({
         name: "Test Kit",
@@ -20,16 +17,68 @@ RSpec.describe Product, type: :model do
         quantity: 4,
         category: @category
       })
-      # we use bang here b/c we want our spec to fail if save fails (due to validations)
-      # we are not testing for successful save so we have to assume it will be successful
       @product.save!
   
       expect(@product.id).to be_present
 
-      expect(@product.name).to be_present
-      expect(@product.price_cents).to be_present
-      expect(@product.quantity).to be_present
-      expect(@product.category).to be_present
+      expect(@product.name).to eq("Test Kit")
+      expect(@product.price_cents).to eq(399)
+      expect(@product.quantity).to eq(4)
+      expect(@product.category).to eq(@category)
+    end
+
+    it 'validates a error for blank name' do
+      @category = Category.create({name: "Spec"})
+      
+      @product = Product.new({
+        price_cents: 420,
+        quantity: 69,
+        category: @category
+      })
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages ).to eq(["Name can't be blank"])
+      expect { @product.save! }.to raise_error(ActiveRecord::RecordInvalid)
+      
+    end
+
+    it 'validates a error for blank price_cents' do
+      @category = Category.create({name: "Spec"})
+      
+      @product = Product.new({
+        name: "Test Kit",
+        quantity: 4,
+        category: @category
+      })
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages ).to eq(["Price cents is not a number", "Price is not a number", "Price can't be blank"])
+      expect { @product.save! }.to raise_error(ActiveRecord::RecordInvalid)
+      
+    end
+
+    it 'validates a error for quantity' do
+      @category = Category.create({name: "Spec"})
+      
+      @product = Product.new({
+        name: "Test Kit",
+        price_cents: 399,
+        category: @category
+      })
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages ).to eq(["Quantity can't be blank"])
+      expect { @product.save! }.to raise_error(ActiveRecord::RecordInvalid)
+      
+    end
+
+    it 'validates a error for category' do
+      @product = Product.new({
+        name: "Test Kit",
+        price_cents: 399,
+        quantity: 4
+      })
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages ).to eq(["Category can't be blank"])
+      expect { @product.save! }.to raise_error(ActiveRecord::RecordInvalid)
+      
     end
   end
 end
